5ac673d0b058ffdbc5c21f9e7c56a6d163a40b94
Andreas Siedler <andreas.siedler@gmail.com>
2020-11-25 14:51:46 +0100

    feat: Added cookie consent

--- web/components/layout.js
+++ web/components/layout.js
@@ -3,6 +3,7 @@ import Footer from './footer'
 import Meta from './meta'
 import Sidebar from './sidebar'
 import ScrollTop from './scroll-top'
+import CookieConsent from "react-cookie-consent";
 
 export default function Layout({ preview, mainMenus, children }) {
   return (
@@ -12,6 +13,7 @@ export default function Layout({ preview, mainMenus, children }) {
         <Header mainMenus={mainMenus} />
         {children}
         <Footer />
+        <CookieConsent>This website uses cookies to enhance the user experience.</CookieConsent>
         <ScrollTop />
         <Sidebar mainMenus={mainMenus} />
       </div>
