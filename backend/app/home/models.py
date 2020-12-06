9407a2f58b672eeca90acd9c9f7d887b817b02a3
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-06 07:59:13 +0000

    cleaned subpage and parent page types

--- backend/app/home/models.py
+++ backend/app/home/models.py
@@ -9,7 +9,7 @@ class HomePage(MetadataPageMixin, FlexPage):
     HomePage model which inherits from Flexpage
     Hint: Here the home template is used which extends the flex_page template
     """
-    subpage_types = ['blog.BlogIndexPage', 'form.FormPage', 'home.SubPage']
+    subpage_types = ['blog.BlogIndexPage', 'home.SubPage']
 
 
 class SubPage(MetadataPageMixin, FlexPage):
@@ -17,5 +17,5 @@ class SubPage(MetadataPageMixin, FlexPage):
     SubPage model which inherits from Flexpage
     Hint: Here the subpage template is used which extends the flex_page template
     """
-    subpage_types = ['blog.BlogIndexPage', 'form.FormPage']
+    subpage_types = ['blog.BlogIndexPage']
     parent_page_types = ['home.HomePage']
