0502047963504b4ccbb13fd558511c5d7b527e4e
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-06 07:59:26 +0000

    fixed singals bug

--- backend/app/profiles/signals.py
+++ backend/app/profiles/signals.py
@@ -3,10 +3,11 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from profiles.models import Visitor, Company
 
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     # print("Created: ", created)
     if created and instance.is_visitor:
-        Visitor.objects.create(user=instance, created_by=instance)
+        Visitor.objects.create(user=instance)
     if created and instance.is_company:
         Company.objects.create(user=instance, created_by=instance)
