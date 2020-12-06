c97ad274609925afdea10bf9a6e65ada1cfecef0
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-06 21:57:37 +0000

    Added axios to api model

--- web/lib/api.js
+++ web/lib/api.js
@@ -1,7 +1,15 @@
 // const API_URL = process.env.BASE_URL || 'http://127.0.0.1:8000'
 // const API_TOKEN = process.env.DATOCMS_API_TOKEN
-
+import Axios from 'axios';
 import { API_URL, API_SERVER_URL } from './constants'
+
+export const api = Axios.create({
+    baseURL: process.env.CLIENT_API_URL,
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
 
 export async function fetchAPI(url, { method, body } = {}) {
     const res = await fetch(process.env.CLIENT_API_URL + url, {
