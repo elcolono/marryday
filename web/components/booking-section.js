c1575ebea2be9efe856c61bbe5fbbf20546844f9
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-08 23:05:19 +0000

    feat: Basic booking section func

--- web/components/booking-section.js
+++ web/components/booking-section.js
@@ -1,8 +1,43 @@
 import { API_SERVER_URL } from '../lib/constants'
-import { Formik, Field, Form } from 'formik'
+import { Formik, Field, Form, useFormikContext } from 'formik'
 import * as Yup from 'yup'
+import { useEffect, useState } from 'react'
+import { api } from '../lib/api'
 
 export default function BookingSection({ data }) {
+
+    const [locations, setLocation] = useState([])
+    const [activeLocation, setActiveLocation] = useState(null)
+    const [rentObjects, setRentObjects] = useState([])
+    const [objectType, setObjectType] = useState(null)
+
+
+    useEffect(() => {
+        api.get('/cowork/locations/').then((response) => {
+            if (response.status == 200) {
+                setLocation(response.data);
+            } else {
+                console.log(response)
+            }
+        })
+    }, [])
+
+
+    const AutoReload = () => {
+        const { values } = useFormikContext();
+        console.log("Autoreload")
+        if (values.objectType !== objectType && activeLocation) {
+            api.get(`/cowork/rentobjects?location=${activeLocation}&type=${values.objectType}`).then((response) => {
+                if (response.status == 200) {
+                    setRentObjects(response.data);
+                } else {
+                    console.log(response)
+                }
+            })
+            setObjectType(values.objectType)
+        }
+        return null
+    }
 
     return (
         <section id="intro_section">
@@ -12,35 +47,17 @@ export default function BookingSection({ data }) {
                         <div className="container container-half">
                             <div className="row">
                                 <div className="col-md-10">
-                                    <h1 className="intro-section-title">{data.heading}</h1>
-                                    <div dangerouslySetInnerHTML={{ __html: data.description }}></div>
-
+                                    <h1 className="intro-section-title">{data.heading} {activeLocation}</h1>
+                                    {/* <div dangerouslySetInnerHTML={{ __html: data.description }}></div> */}
                                     <Formik
                                         initialValues={{
                                             objectType: 'desktop',
                                             date: '',
-                                            email: '',
-                                            password: ''
                                         }}
-                                        validationSchema={Yup.object({
-                                            email: Yup.string()
-                                                .email('Ungültige Email Adresse')
-                                                .required('Erforderlich'),
-                                            password: Yup.string()
-                                                .max(15, 'Ungültiges Passwort')
-                                                .required('Erforderlich'),
-                                        })}
                                         onSubmit={(values, { setSubmitting, setStatus }) => {
                                             setStatus(false)
                                             setTimeout(() => {
-                                                login(values.email, values.password).then((response) => {
-                                                    if (response.error) {
-                                                        setStatus("Fehler bei Anmeldung")
-                                                    } else {
-                                                        setStatus("Erfolgreich angemeldet")
-                                                    }
-                                                    setSubmitting(false);
-                                                })
+
                                             }, 400);
                                         }}
                                     >
@@ -85,51 +102,13 @@ export default function BookingSection({ data }) {
                                                         <div className="text-danger position-absolute">{errors.date && touched.date && errors.date}</div>
                                                     </div>
 
-                                                    <div className="form-group">
-                                                        <label htmlFor="email" className="form-control-label">Email</label>
-                                                        <input
-                                                            className="form-control"
-                                                            placeholder="Email Adresse"
-                                                            type="email"
-                                                            name="email"
-                                                            onChange={handleChange}
-                                                            onBlur={handleBlur}
-                                                            value={values.email}
-                                                        />
-                                                        <div className="text-danger position-absolute">{errors.email && touched.email && errors.email}</div>
-                                                    </div>
-                                                    <div className="form-group">
-                                                        <label htmlFor="password" className="form-control-label">Passwort</label>
+                                                    {/* Rent Objects */}
+                                                    <AutoReload />
+                                                    {JSON.stringify(rentObjects)}
 
-                                                        <div className="form-control-icon form-control-icon_right">
-                                                            <input
-                                                                id="password1"
-                                                                className="form-control"
-                                                                placeholder="Passwort"
-                                                                type="password"
-                                                                name="password"
-                                                                onChange={handleChange}
-                                                                onBlur={handleBlur}
-                                                                value={values.password}
-                                                            />
-                                                            <button type="button" className="form-control-icon_wrapper icon-eye">
-                                                                <span>
-                                                                    <i className="eye-open ion-md-eye"></i>
-                                                                    <i className="eye-close ion-md-eye-off d-none"></i>
-                                                                </span>
-                                                            </button>
-                                                        </div>
 
-                                                        <div className="text-danger position-absolute">{errors.password && touched.password && errors.password}</div>
-                                                    </div>
-                                                    <div className="form-group d-flex align-items-center justify-content-between">
-                                                        <div className="checkbox">
-                                                            <input id='remember' type='checkbox' />
-                                                            <label htmlFor="remember">Remember me</label>
-                                                        </div>
-                                                        <a href="#" className="link" data-dismiss="modal" data-toggle="modal" data-target="#forgot">Forgot Password?</a>
-                                                    </div>
-                                                    <button type="submit" className="btn btn-danger btn-block">Sign in</button>
+                                                    {/* BOOKING BUTTON */}
+                                                    <button type="submit" className="btn btn-danger btn-block">Buchen</button>
                                                     <div className="text-danger mt-2">{status && status}</div>
                                                     {JSON.stringify(values)}
                                                 </Form>
@@ -140,7 +119,13 @@ export default function BookingSection({ data }) {
                         </div>
                     </div>
                 </div>
-                <div style={data.image ? { backgroundImage: `url(${API_SERVER_URL + data.image.url})` } : null} className="col-lg-6 banner inner-banner overlay-banner-ipad hero-about"></div>
+                <div className="col-lg-6 mt-5">
+                    <ul className="mt-5">
+                        {locations && locations.map((location, i) =>
+                            <li key={i} onClick={() => setActiveLocation(location.id)}>{location.title}</li>
+                        )}
+                    </ul>
+                </div>
             </div>
         </section>
     )
