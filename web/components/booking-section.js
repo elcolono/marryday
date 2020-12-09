1a79c5de5890ad94a10c293377fe7ddc385a8eb9
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-09 17:33:42 +0000

    feat: Bookingsystem with datefilter

--- web/components/booking-section.js
+++ web/components/booking-section.js
@@ -7,7 +7,6 @@ import { api } from '../lib/api'
 export default function BookingSection({ data }) {
 
     const [locations, setLocation] = useState([])
-    const [activeLocation, setActiveLocation] = useState(null)
     const [rentObjects, setRentObjects] = useState([])
 
     useEffect(() => {
@@ -28,7 +27,7 @@ export default function BookingSection({ data }) {
         values[name] = value
         setValues(values)
         if (values.fm_location) {
-            api.get(`/cowork/rentobjects?location=${values.fm_location.id}&type=${values.fm_objectType}`).then((response) => {
+            api.get(`/cowork/rentobjects?location=${values.fm_location.id}&type=${values.fm_objectType}&date=${values.fm_date}`).then((response) => {
                 if (response.status == 200) {
                     setRentObjects(response.data);
                 } else {
@@ -44,8 +43,10 @@ export default function BookingSection({ data }) {
                 initialValues={{
                     fm_location: '',
                     fm_objectType: 'desktop',
-                    fm_date: '',
+                    fm_date: '2020-12-09',
                     fm_rentObject: '',
+                    fm_startTime: '',
+                    fm_endTime: '',
                 }}
                 onSubmit={(values, { setSubmitting, setStatus }) => {
                     setStatus(false)
@@ -55,8 +56,6 @@ export default function BookingSection({ data }) {
             >
                 {({
                     values,
-                    errors,
-                    touched,
                     handleBlur,
                     handleSubmit,
                     isSubmitting,
@@ -65,62 +64,78 @@ export default function BookingSection({ data }) {
                     /* and other goodies */
                 }) => (
                         <div className="row no-gutters coming-soon">
-                            <div className="col-lg-6">
-                                <div className="banner-content banner-content-white">
-                                    <div className="container container-half">
-                                        <div className="row">
-                                            <div className="col-md-10">
-                                                <h1 className="intro-section-title">{activeLocation && activeLocation.title}</h1>
-                                                {/* <h1 className="intro-section-title">{data.heading}</h1> */}
-                                                {/* <div dangerouslySetInnerHTML={{ __html: data.description }}></div> */}
-                                                <Form onSubmit={handleSubmit} className="pt-3">
-                                                    <div className="form-group">
-                                                        <div class="radio">
-                                                            <Field onChange={(e) => customHandleChange("desktop", "fm_objectType", values, setValues)} value="desktop" name="fm_objectType" type="radio" id="radio1" />
-                                                            <label htmlFor="radio1">Dektop</label>
-                                                        </div>
-                                                        <div class="radio">
-                                                            <Field onChange={(e) => customHandleChange("phone", "fm_objectType", values, setValues)} value="phone" name="fm_objectType" type="radio" id="radio2" />
-                                                            <label htmlFor="radio2">Phone</label>
-                                                        </div>
-                                                        <div class="radio">
-                                                            <Field onChange={(e) => customHandleChange("meeting", "fm_objectType", values, setValues)} value="meeting" name="fm_objectType" type="radio" id="radio3" />
-                                                            <label htmlFor="radio3">Meeting</label>
-                                                        </div>
-                                                    </div>
-
-                                                    <div className="form-group">
-                                                        <label htmlFor="date" className="form-control-label">Date</label>
-                                                        <input
-                                                            className="form-control"
-                                                            placeholder="Datum"
-                                                            type="date"
-                                                            onChange={(e) => customHandleChange(e.target.value, "fm_date", values, setValues)}
-                                                            onBlur={handleBlur}
-                                                            value={values.date}
-                                                        />
-                                                        <div className="text-danger position-absolute">{errors.date && touched.date && errors.date}</div>
-                                                    </div>
-
-                                                    {/* Rent Objects */}
-                                                    <div className="form-group">
-                                                        {rentObjects && rentObjects.map((rentObject, i) => (
-                                                            <label key={i}>
-                                                                <Field type="radio" name="fm_rentObject" value={`${rentObject.id}`} />{rentObject.title}
-                                                            </label>
-                                                        ))}
-                                                    </div>
-
-                                                    {JSON.stringify(rentObjects)}
-
-
-                                                    {/* BOOKING BUTTON */}
-                                                    <button type="submit" className="btn btn-danger btn-block">Buchen</button>
-                                                    <div className="text-danger mt-2">{status && status}</div>
-                                                    {JSON.stringify(values)}
-                                                </Form>
+                            <div className="col-lg-6 p-5">
+                                <div className="row">
+                                    <div className="col-md-10">
+                                        <h1 className="intro-section-title">{values.fm_location && values.fm_location.title}</h1>
+                                        {/* <h1 className="intro-section-title">{data.heading}</h1> */}
+                                        {/* <div dangerouslySetInnerHTML={{ __html: data.description }}></div> */}
+                                        <Form onSubmit={handleSubmit} className="pt-3">
+                                            <div className="form-group">
+                                                <div class="radio">
+                                                    <Field onChange={() => customHandleChange("desktop", "fm_objectType", values, setValues)} value="desktop" name="fm_objectType" type="radio" id="radio1" />
+                                                    <label htmlFor="radio1">Dektop</label>
+                                                </div>
+                                                <div class="radio">
+                                                    <Field onChange={() => customHandleChange("phone", "fm_objectType", values, setValues)} value="phone" name="fm_objectType" type="radio" id="radio2" />
+                                                    <label htmlFor="radio2">Phone</label>
+                                                </div>
+                                                <div class="radio">
+                                                    <Field onChange={() => customHandleChange("meeting", "fm_objectType", values, setValues)} value="meeting" name="fm_objectType" type="radio" id="radio3" />
+                                                    <label htmlFor="radio3">Meeting</label>
+                                                </div>
                                             </div>
-                                        </div>
+
+                                            <div className="form-group">
+                                                <label htmlFor="date" className="form-control-label">Date</label>
+                                                <input
+                                                    className="form-control"
+                                                    placeholder="Datum"
+                                                    type="date"
+                                                    onChange={(e) => customHandleChange(e.target.value, "fm_date", values, setValues)}
+                                                    onBlur={handleBlur}
+                                                    value={values.fm_date}
+                                                />
+                                            </div>
+                                            <div className="form-group">
+                                                <label htmlFor="date" className="form-control-label">Start / End</label>
+                                                <input
+                                                    className="form-control"
+                                                    placeholder="Datum"
+                                                    type="time"
+                                                    onChange={(e) => customHandleChange(e.target.value, "fm_startTime", values, setValues)}
+                                                    onBlur={handleBlur}
+                                                    value={values.fm_startTime}
+                                                />
+                                                <input
+                                                    className="form-control"
+                                                    placeholder="Datum"
+                                                    type="time"
+                                                    onChange={(e) => customHandleChange(e.target.value, "fm_endTime", values, setValues)}
+                                                    onBlur={handleBlur}
+                                                    value={values.fm_endTime}
+                                                />
+                                            </div>
+
+                                            {/* Rent Objects */}
+                                            <div className="form-group">
+                                                {rentObjects && rentObjects.map((rentObject, i) => (
+                                                    <div key={i}>
+                                                        <label>
+                                                            <Field type="radio" name="fm_rentObject" value={`${rentObject.id}`} />
+                                                            {rentObject.title} ({rentObject.bookings.map(booking => (`${booking.start} - ${booking.end}`))})
+                                                        </label>
+                                                    </div>
+                                                ))}
+                                            </div>
+
+                                            {/* {JSON.stringify(rentObjects)} */}
+
+                                            {/* BOOKING BUTTON */}
+                                            <button type="submit" className="btn btn-danger btn-block">Buchen</button>
+                                            <div className="text-danger mt-2">{status && status}</div>
+                                            {JSON.stringify(values)}
+                                        </Form>
                                     </div>
                                 </div>
                             </div>
