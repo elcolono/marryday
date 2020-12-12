7440653babab35303e54650f489c692c8831e693
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-12 20:04:37 +0100

    Temp: Updated local qpi_server_url

--- web/lib/constants.js
+++ web/lib/constants.js
@@ -1,6 +1,9 @@
 // https://medium.com/frontend-digest/environment-variables-in-next-js-9a272f0bf655
 // NO SECRETS OR API KEYS HERE
 // On the right side define local env values ( because locally we dont use env files )
+
+// TODO DEFINE CORRECT BROWSER VARIABLES
+
 export const CMS_NAME = 'MOWO Spaces'
 export const API_URL = process.env.BASE_URL || 'http://127.0.0.1:8000'
-export const API_SERVER_URL = process.env.BASE_SERVER_URL || 'http://127.0.0.1:8000'
+export const API_SERVER_URL = process.env.BASE_SERVER_URL || 'http://135.181.97.12:8000'
