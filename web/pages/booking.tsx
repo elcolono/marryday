7ce7608fd9f15db7ac74789e0afef3c78b001f8e
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-15 17:03:02 +0100

    Converted booking to typecript component

--- web/pages/booking.tsx
+++ web/pages/booking.tsx
@@ -2,23 +2,26 @@ import { useEffect, useState } from 'react'
 import { api, fetchAPIwithSSR } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
+import { Location, RentObject } from '../lib/interfaces'
+
 import dynamic from "next/dynamic";
 
 import { Form, FormGroup, Label, Input, Spinner, Button } from "reactstrap";
 
 import Layout from '../components/layout'
 import Head from 'next/head'
+import { GetServerSideProps } from 'next';
 
 export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
 
-    const [locations, setLocations] = useState(false)
-    const [location, setLocation] = useState(false)
+    const [locations, setLocations] = useState<Array<Location>>(undefined)
+    const [location, setLocation] = useState<Location>(undefined)
     const [objectType, setObjectType] = useState('desktop')
     const [startTime, setStartTime] = useState(`${new Date().getHours()}:${new Date().getMinutes()}`)
     const [date, setDate] = useState('2020-12-09')
     const [duration, setDuration] = useState(60)
-    const [rentObjects, setRentObjects] = useState(false)
-    const [rentObject, setRentObject] = useState(false)
+    const [rentObjects, setRentObjects] = useState<Array<RentObject>>(undefined)
+    const [rentObject, setRentObject] = useState("")
 
     const [isLoading, setIsLoading] = useState(false)
 
@@ -34,15 +37,21 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
         }
     }, [])
 
-    useEffect(async () => {
-        if (location) {
-            try {
-                const response = await api.get(`/cowork/rentobjects?location=${location.id}&type=${objectType}&start=${date}T${startTime}Z&duration=${duration}`)
-                setRentObjects(response.data);
-            } catch (error) {
-                console.log(error)
+    useEffect(() => {
+
+        const fetchRentObjects = async () => {
+            if (location) {
+                try {
+                    const response = await api.get(`/cowork/rentobjects?location=${location.id}&type=${objectType}&start=${date}T${startTime}Z&duration=${duration}`)
+                    setRentObjects(response.data);
+                } catch (error) {
+                    console.log(error)
+                }
             }
         }
+
+        fetchRentObjects()
+
     }, [location, objectType, date, startTime, duration])
 
     async function handleSubmit(e) {
@@ -92,7 +101,7 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
                                         <div className="col-md-10">
                                             <h1 className="intro-section-title">Ups.</h1>
                                             <div>Sie sind leider nicht angemeldet. Wenn Sie eine Buchung durchführen möchten melden Sie sich bitte an.</div>
-                                            <a href="/signin" class="mt-4 btn btn-pill btn-danger"><span>Jetzt anmelden</span></a>
+                                            <a href="/signin" className="mt-4 btn btn-pill btn-danger"><span>Jetzt anmelden</span></a>
                                         </div>
                                     </div>
                                 </div>
@@ -104,6 +113,18 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
             ) : (
                     <section id="intro_section">
                         <div className="row no-gutters coming-soon">
+                            <div className="col-lg-6 banner inner-banner overlay-banner-ipad">
+                                <div id="map">
+                                    <MapWithNoSSR locations={locations} />
+                                </div>
+                                <ul className="mt-5">
+                                    {locations && locations.map((location, i) =>
+                                        <li key={i} onClick={() => setLocation(location)}>
+                                            {location.title} ({location.address}, {location.postcode}, {location.city})
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
                             <div className="col-lg-6 p-5">
                                 <div className="row">
                                     <div className="col-md-10">
@@ -111,7 +132,38 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
                                         <Form onSubmit={handleSubmit} className="pt-3">
                                             <FormGroup tag="fieldset">
                                                 <legend>Object Type</legend>
-                                                <FormGroup check>
+
+                                                <div className="row">
+                                                    <div className="col-md-4" onClick={() => handleValueChange('desktop', 'objectType')} >
+                                                        <div className="card overflow-hidden dashboard--card bg-danger text-white border-0">
+                                                            <div className="dashboard--card__icon"><i className="ion-md-list"></i></div>
+                                                            <div className="card-body">
+                                                                <h2 className="mb-0 text-white">Desktop</h2>
+                                                                <p>Total Listing</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-4" onClick={() => handleValueChange('phone', 'objectType')}>
+                                                        <div className="card overflow-hidden dashboard--card bg-warning border-0">
+                                                            <div className="dashboard--card__icon"><i className="ion-md-star"></i></div>
+                                                            <div className="card-body">
+                                                                <h2 className="mb-0">Phone</h2>
+                                                                <p>Total Reviews</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-md-4" onClick={() => handleValueChange('meeting', 'objectType')}>
+                                                        <div className="card overflow-hidden dashboard--card">
+                                                            <div className="dashboard--card__icon"><i className="ion-md-bookmark"></i></div>
+                                                            <div className="card-body">
+                                                                <h2 className="mb-0">Meeting</h2>
+                                                                <p>Total Bookmarked</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                {/* <FormGroup check>
                                                     <Label check>
                                                         <Input onChange={() => handleValueChange('desktop', 'objectType')} checked={objectType === 'desktop'} type="radio" />{' '}
                                         Desktop
@@ -128,7 +180,7 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
                                                         <Input onChange={() => handleValueChange('meeting', 'objectType')} checked={objectType === 'meeting'} type="radio" />{' '}
                                         Meeting
                                     </Label>
-                                                </FormGroup>
+                                                </FormGroup> */}
                                             </FormGroup>
 
                                             <FormGroup>
@@ -165,18 +217,6 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-lg-6 mt-5">
-                                <div id="map">
-                                    <MapWithNoSSR locations={locations} />
-                                </div>
-                                <ul className="mt-5">
-                                    {locations && locations.map((location, i) =>
-                                        <li key={i} onClick={() => setLocation(location)}>
-                                            {location.title} ({location.address}, {location.postcode}, {location.city})
-                        </li>
-                                    )}
-                                </ul>
-                            </div>
                         </div>
                     </section >
                 )}
@@ -188,11 +228,11 @@ export default function Booking({ user, mainMenus, flatMenus, themeSettings }) {
 }
 
 // If you export an async function called getStaticProps from a page, Next.js will pre-render this page at build time using the props returned by getStaticProps.
-export async function getServerSideProps({ req }) {
-    const user = (await fetchAPIwithSSR('/api/v1/rest-auth/user/', { req: req })) ?? null
-    const mainMenus = (await fetchAPIwithSSR('/api/main-menus', { method: 'GET' })) ?? []
-    const flatMenus = (await fetchAPIwithSSR('/api/flat-menus', { method: 'GET' })) ?? []
-    const themeSettings = (await fetchAPIwithSSR('/api/theme-settings', { method: 'GET' })) ?? []
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const user = (await fetchAPIwithSSR('/api/v1/rest-auth/user/', { method: 'GET', req: req })) ?? null
+    const mainMenus = (await fetchAPIwithSSR('/api/main-menus', { method: 'GET', req: req })) ?? []
+    const flatMenus = (await fetchAPIwithSSR('/api/flat-menus', { method: 'GET', req: req })) ?? []
+    const themeSettings = (await fetchAPIwithSSR('/api/theme-settings', { method: 'GET', req: req })) ?? []
     return {
         props: { user, mainMenus, flatMenus, themeSettings },
     }
