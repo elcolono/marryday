264ea8f4fc7c4489d851287790c74a8d3c5356ac
Andreas Siedler <andreas.siedler@gmail.com>
2020-12-29 20:45:44 +0000

    Improved timerangeslider

--- web/components/TimeRangeSlider.tsx
+++ web/components/TimeRangeSlider.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 
 import addHours from 'date-fns/addHours'
+import subHours from 'date-fns/subHours'
 import isWithinInterval from 'date-fns/isWithinInterval'
 import roundToNearestMinutes from 'date-fns/roundToNearestMinutes'
 import addMinutes from 'date-fns/addMinutes'
 import isBefore from 'date-fns/isBefore'
 import isAfter from 'date-fns/isAfter'
+import format from "date-fns/format";
 
 import TimeRange from 'react-timeline-range-slider'
 import Draggable from 'react-draggable';
@@ -14,6 +16,11 @@ import { Button } from 'reactstrap'
 
 
 const now = new Date()
+const nearestHour = roundToNearestMinutes(now, { nearestTo: 30 })
+const startTimeLinePosition = -750
+const stepInterval = 250;
+
+
 const getInitInterval = () => {
     // Get nearest minutes - nearest to 30 min
     // But if takes lower halfhour than add 60 minutes to be before now
@@ -34,65 +41,11 @@ const TimeRangeSlider = ({
     error
 }) => {
 
-    const [timeRangeIndex, setTimeRangeIndex] = useState(0)
-    const [deltaPosition, setDeltaPosition] = useState({
-        x: 0, y: 0
-    })
+    const [timelineInterval, setTimelineInterval] = useState([subHours(nearestHour, 12), addHours(nearestHour, 12)])
+    const [timelinePosition, setTimelinePosition] = useState(startTimeLinePosition)
+
     const [isLoading, setIsLoading] = useState(false)
 
-    useEffect(() => {
-        setIsLoading(true)
-        if (deltaPosition.x > (timeRangeIndex * 250) * -1) {
-            var position = deltaPosition.x;
-            var id = setInterval(frame, 0.1);
-        } else {
-            var position = deltaPosition.x;
-            var id = setInterval(framemin, 0.1);
-        }
-
-        function frame() {
-            if (position == (timeRangeIndex * 250) * -1) {
-                setIsLoading(false)
-                clearInterval(id);
-
-            } else {
-                position = position - 5;
-                setDeltaPosition({
-                    x: position,
-                    y: 0,
-                });
-            }
-        }
-        function framemin() {
-            if (position == (timeRangeIndex * 250) * -1) {
-                setIsLoading(false)
-                clearInterval(id);
-
-            } else {
-                position = position + 5;
-                setDeltaPosition({
-                    x: position,
-                    y: 0,
-                });
-            }
-        }
-
-    }, [timeRangeIndex])
-
-    useEffect(() => {
-        // Check if current time is wihtin timerangeindex interval 
-        // if wihtin interval(true) set Time range index to the value
-        for (let i = 0; i <= 5; i++) {
-            const timeRangeIndex = i
-            const isWithin: boolean = isWithinInterval(now, {
-                start: addHours(selectedDate, timeRangeIndex * 4),
-                end: addHours(selectedDate, (timeRangeIndex * 4) + 8)
-            })
-            if (isWithin) {
-                setTimeRangeIndex(timeRangeIndex)
-            }
-        }
-    }, [])
 
     const getDisabledIntervals = () => {
         const intervals: Array<any> = disabledIntervals.map(iv => {
@@ -109,44 +62,46 @@ const TimeRangeSlider = ({
     }
 
     const increaseTimeRangeIndex = () => {
-        let index = timeRangeIndex;
-        if (timeRangeIndex < 5) {
-            index++
-            setTimeRangeIndex(index)
-        } else if (index >= 5) {
-            setTimeRangeIndex(0)
-            increaseSelectedDay()
+        setIsLoading(true)
+
+        // Set new time Interval
+        setTimelineInterval([addHours(timelineInterval[0], 5), addHours(timelineInterval[1], 5)])
+        // Get back to position
+        setTimelinePosition(timelinePosition + stepInterval)
+
+        var position = startTimeLinePosition + stepInterval;
+        var id = setInterval(frame, 0.1);
+
+        function frame() {
+            if (position == startTimeLinePosition) {
+                setIsLoading(false)
+                clearInterval(id);
+            } else {
+                position = position - 5;
+                setTimelinePosition(position);
+            }
         }
     }
 
     const decreaseTimeRangeIndex = () => {
-        // First check if TimeRange is selectable
-        // If NOT stop this function
-        let index = timeRangeIndex;
-        if (!checkTimeRangeSelectable(index - 1)) {
-            return
-        }
-        if (timeRangeIndex > 0) {
-            index--
-            setTimeRangeIndex(index)
-        } else if (timeRangeIndex <= 0) {
-            setTimeRangeIndex(5)
-            decreaseSelectedDay()
-        }
-    }
+        setIsLoading(true)
+        // Set new time Interval
+        setTimelineInterval([subHours(timelineInterval[0], 5), subHours(timelineInterval[1], 5)])
+        // Get back to position
+        setTimelinePosition(timelinePosition - stepInterval)
 
-    const checkTimeRangeSelectable = (timeRangeIndex) => {
-        // Check if now is wihtin timeRange Interval
-        const isWithin: boolean = isWithinInterval(now, {
-            start: addHours(selectedDate, timeRangeIndex * 4),
-            end: addHours(selectedDate, (timeRangeIndex * 4) + 8)
-        })
-        // Return true if now is within interval
-        if (isWithin) { return true }
-        // Else check if timerange start is after now
-        const after: boolean = isAfter(addHours(selectedDate, timeRangeIndex * 4), now);
-        if (after) { return true }
-        return false
+        var position = startTimeLinePosition - stepInterval;
+        var id = setInterval(frame, 0.1);
+
+        function frame() {
+            if (position == startTimeLinePosition) {
+                setIsLoading(false)
+                clearInterval(id);
+            } else {
+                position = position + 5;
+                setTimelinePosition(position);
+            }
+        }
     }
 
     const onChangeTimeInterval = (ti) => {
@@ -155,41 +110,30 @@ const TimeRangeSlider = ({
         // setLastAction(undefined)
     }
 
-    const handleDrag = (e, ui) => {
-        const { x, y } = deltaPosition;
-        setDeltaPosition({
-            x: x + ui.deltaX,
-            y: y + ui.deltaY,
-        });
-    };
-
     return (
         <React.Fragment>
             <Button
                 disabled={isLoading}
                 color="items"
                 className="btn-items-increase m-1"
-                onClick={decreaseTimeRangeIndex}
+                onClick={() => decreaseTimeRangeIndex()}
             ><i className="fa fa-chevron-left"></i></Button>
             <Button
                 disabled={isLoading}
                 color="items"
                 className="btn-items-increase m-1"
-                onClick={increaseTimeRangeIndex}
+                onClick={() => increaseTimeRangeIndex()}
             ><i className="fa fa-chevron-right"></i></Button>
 
             <div className="d-flex align-items-center">
                 <Draggable
                     axis="x"
                     handle=".handle"
-                    defaultPosition={{ x: 0, y: 0 }}
+                    // defaultPosition={{ x: 0, y: 0 }}
                     // bounds={{ top: -100, left: -1000, right: 0, bottom: 100 }}
-                    position={deltaPosition}
+                    position={{ x: timelinePosition, y: 0 }}
                     grid={[25, 25]}
                     scale={1}
-                    // onStart={this.handleStart}
-                    onDrag={handleDrag}
-                // onStop={this.handleStop}
                 >
                     <div>
                         {/* <div className="handle">Drag from here</div> */}
@@ -198,7 +142,8 @@ const TimeRangeSlider = ({
                             ticksNumber={36}
                             selectedInterval={selectedInterval}
                             // timelineInterval={[addHours(selectedDate, timeRangeIndex * 4), addHours(selectedDate, (timeRangeIndex * 4) + 8)]}
-                            timelineInterval={[startOfDay(selectedDate), endOfDay(selectedDate)]}
+                            // timelineInterval={[startOfDay(selectedDate), endOfDay(selectedDate)]}
+                            timelineInterval={timelineInterval}
                             onUpdateCallback={errorHandler}
                             onChangeCallback={onChangeTimeInterval}
                             disabledIntervals={getDisabledIntervals()}
@@ -208,7 +153,10 @@ const TimeRangeSlider = ({
 
 
             </div>
-            {/* <div>x: {deltaPosition.x.toFixed(0)}, y: {deltaPosition.y.toFixed(0)}</div> */}
+            <div>x: {timelinePosition}</div>
+            <div>start: {format(selectedInterval[0], "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")}</div>
+            <div>end: {format(selectedInterval[1], "yyyy-MM-dd'T'HH:mm:ss.SSSxxx")}</div>
+
         </React.Fragment>
     )
 }
