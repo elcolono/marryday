9b8a2620f31a79c45cb9acf090f1ca30bc823d1a
Andreas Siedler <andreas.siedler@gmail.com>
2021-01-17 12:24:06 +0000

    Update pricing section

--- web/components/Section/Pricing.tsx
+++ web/components/Section/Pricing.tsx
@@ -9,6 +9,7 @@ import {
     Button,
 } from "reactstrap"
 
+import Link from 'next/link';
 
 export default function PricingSection({ data }) {
     return (
@@ -18,17 +19,14 @@ export default function PricingSection({ data }) {
                     {data.pricings &&
                         data.pricings.map((column, index) => (
                             <Col key={index} lg="4">
-                                <Card
-                                    className={`mb-5 mb-lg-0 border-0 ${index === 1 ? "card-highlight shadow-lg" : "shadow"
-                                        }`}
-                                >
+                                <Card className={`mb-5 mb-lg-0 border-0 ${index === 1 ? "card-highlight shadow-lg" : "shadow"}`}>
                                     {index === 1 && <div className="card-status bg-primary" />}
                                     <CardBody>
                                         <h2 className="text-base subtitle text-center text-primary py-3">
                                             {column.heading}
                                         </h2>
                                         <p className="text-muted text-center">
-                                            <span className="h1 text-dark">€ {column.price}</span>
+                                            <span className="h1 text-dark">{column.price}</span>
                                             <span className="ml-2">/ Stunde</span>
                                         </p>
                                         <hr />
@@ -55,10 +53,15 @@ export default function PricingSection({ data }) {
                                             ))}
                                         </ul>
                                         <div className="text-center">
-                                            <Button href="#" color="outline-primary">
-                                                Select
-                        </Button>
+                                            <Link href={`/locations`} passHref>
+                                                <a>
+                                                    <Button color="outline-primary">
+                                                        Auswählen
+                                                    </Button>
+                                                </a>
+                                            </Link>
                                         </div>
+
                                     </CardBody>
                                 </Card>
                             </Col>
@@ -66,45 +69,5 @@ export default function PricingSection({ data }) {
                 </Row>
             </Container>
         </section>
-        // <section className="section">
-        //     <div className="container">
-        //         <div className="row">
-        //             <div className="col-xl-6 col-lg-8 col-md-11">
-        //                 <h2 className="section-title"><span>{data.heading}</span></h2>
-        //             </div>
-        //         </div>
-        //         <div className="row align-items-end">
-        //             <div className="col-lg-3 pb-4">
-        //                 {/* <h4 className="mb-3">Free Plan</h4> */}
-        //                 <div className="mb-4" dangerouslySetInnerHTML={{ __html: data.description }}></div>
-        //                 <img src={process.env.CLIENT_API_URL + data.image.url} alt=""></img>
-        //                 {/* <a href="#" className="btn btn-danger">Register</a> */}
-        //             </div>
-        //             <div className="col-lg-9">
-        //                 <div className="row no-gutters">
-
-        //                     {data.pricings.map((pricing) => (
-        //                         <div className="col-md-4 price-card">
-        //                             <div className="price-card-header">
-        //                                 <h4 className="mb-3">{pricing.heading}</h4>
-        //                                 <span className="price"><span>€{pricing.price}</span>/{pricing.type == 'hourly' ? "Stunde" : "Tag"}</span>
-        //                             </div>
-        //                             <div className="price-text mb-4" dangerouslySetInnerHTML={{ __html: pricing.description }}>
-        //                             </div>
-        //                             {/* <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium</p>
-        //                             <ul className="price-text">
-        //                                 <li>Neque porro quisquam est qui dolorem</li>
-        //                                 <li>Ut enim ad minima veniam</li>
-        //                                 <li>Quis autem vel eum iure</li>
-        //                                 <li>Et harum quidem rerum facilis est et expedita distinctio.</li>
-        //                             </ul> */}
-        //                             <a href="/contact" className="btn btn-danger">Get in Touch</a>
-        //                         </div>
-        //                     ))}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </section>
     )
 }
