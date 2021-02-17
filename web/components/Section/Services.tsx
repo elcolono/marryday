62ba8974bfcf62cd77679eb89484e8a443af9a27
Andreas Siedler <andreas.siedler@gmail.com>
2021-02-17 23:23:13 +0000

    Reworked Service Section

--- web/components/Section/Services.tsx
+++ web/components/Section/Services.tsx
@@ -11,7 +11,7 @@ import Icon from "../Icon"
 export default function ServiceSection({ data }) {
     return (
 
-        <section className="py-6">
+        <section className="py-6 bg-gray-100">
             <Container>
                 <div className="text-center pb-lg-4">
                     {data.subheading && <p className="subtitle text-secondary">
@@ -21,24 +21,30 @@ export default function ServiceSection({ data }) {
                 </div>
                 <Row>
                     {data.services && data.services.map((service, i) => (
-                        <Col key={i} md="4" className="text-center text-md-left mb-4 mb-md-0">
-                            {data.layout == "service_with_icon" && (
-                                <div className="icon-rounded mb-4 bg-primary-light">
-                                    <Icon
-                                        icon="map-location-1"
-                                        className="w-2rem h-2rem text-primary"
-                                    />
-                                </div>
-                            )}
-                            {data.layout == "service_with_image" && (
-                                <div className="icon-image-rounded mb-4 bg-primary-light">
-                                    <img src={service.image.url} alt=""></img>
-                                </div>
-                            )}
-                            <h3 className="h5">{service.heading}</h3>
-                            <p className="text-muted">
-                                {service.description}
-                            </p>
+                        <Col
+                            key={service.heading}
+                            md="4"
+                            className="mb-3 mb-lg-0 text-center"
+                        >
+                            <div className="px-0 px-lg-3">
+                                {data.layout == "service_with_icon" && (
+                                    <div className="icon-rounded bg-primary-light mb-3">
+                                        <Icon
+                                            icon={service.icon}
+                                            className="text-primary w-2rem h-2rem"
+                                        />
+                                    </div>
+                                )}
+                                {data.layout == "service_with_image" && (
+                                    <div className="icon-image-rounded mb-4 bg-primary-light">
+                                        <img src={service.image.url} alt=""></img>
+                                    </div>
+                                )}
+                                <h3 className="h5">{service.heading}</h3>
+                                <p className="text-muted">
+                                    {service.description}
+                                </p>
+                            </div>
                         </Col>
                     ))}
                 </Row>
