5630f830d4001d12d48fae7df6c0034a126037bc
Andreas Siedler <andreas.siedler@gmail.com>
2021-02-19 16:54:08 +0000

    Added Webpack Analyze bundler

--- web/next.config.js
+++ web/next.config.js
@@ -16,6 +16,10 @@ const webpackConfig = {
         return config
     },
 }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
 module.exports = withPlugins(
     [
         [
