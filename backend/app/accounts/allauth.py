9dd37fd908113787c56dbbeb889c56edc6048514
Andreas Siedler <andreas.siedler@gmail.com>
2021-03-15 20:00:48 +0000

    Updated Signup Confirmation Email

--- backend/app/accounts/allauth.py
+++ backend/app/accounts/allauth.py
@@ -4,6 +4,17 @@ from django.conf import settings
 
 class AccountAdapter(DefaultAccountAdapter):
 
+    def send_mail(self, template_prefix, email, context):
+        if not settings.DEBUG:
+            context['activate_url'] = settings.CLIENT_DOMAIN + \
+                'api/v1/accounts-rest/registration/account-confirm-email/' + \
+                context['key'] + '/'
+
+        msg = self.render_mail(template_prefix, email, context)
+        msg.subject = 'Erfolgreiche Registrierung. Bitte bestätigen Sie Ihre E-Mail Adresse.'
+        msg.from_email = 'MoWo Spaces<no-reply@mowo.space>'
+        msg.send()
+
     def get_email_confirmation_redirect_url(self, request):
         if settings.DEBUG:
             return 'http://localhost:3002/login?verification=1'
