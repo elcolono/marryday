42fa7f4956a9a7168450e66428ed7ad08c5a7974
Andreas Siedler <andreas.siedler@gmail.com>
2021-06-23 16:48:50 +0200

    WIP

--- backend/app/accounts/urls.py
+++ backend/app/accounts/urls.py
@@ -1,8 +1,11 @@
-""" Accout urls """
-from django.urls import path
+""" Account urls """
+from django.urls import path, include
 
 from .views import UserView
 
 urlpatterns = [
-    path("user/<int:pk>/", UserView.as_view(), name="user-detail"),
+    path('auth/', include('rest_auth.urls')),
+    path('auth/registration/',
+         include('rest_auth.registration.urls')),
+    path('user/<int:pk>/', UserView.as_view(), name="user-detail"),
 ]
